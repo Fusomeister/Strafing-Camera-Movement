569c77a49057de03e467021ff733462150020d23
Fusomeister <stofferdude@hotmail.com>
2017-02-17 15:35:50 +0100

    init
    init

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public GameObject target;
+    //Vector3 offset;
+
+    private float leftCamera = -2f;
+    private float rightCamera = 2f;
+    private float currentLocation;
+    public float cameraSpeed = 2f;
+
+    void Start()
+    {
+        currentLocation = transform.localPosition.x; 
+    }
+
+    void Update()
+    {
+        currentLocation += Input.GetAxis("Horizontal") * cameraSpeed;
+        Debug.Log(currentLocation);
+
+        if (currentLocation < leftCamera)
+        {
+            currentLocation = leftCamera;
+        }
+        if (currentLocation > rightCamera)
+        {
+            currentLocation = rightCamera;
+        }
+
+        transform.localPosition = new Vector3(currentLocation, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    void LateUpdate()
+    {
+        //float desiredAngle = target.transform.eulerAngles.y;
+        //Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+
+        //transform.position = target.transform.position - (rotation * offset);
+        //transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/moveCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCharacter : MonoBehaviour {
+
+    private float turnSpeed = 10f;
+    private float moveSpeed = 10f;
+    private float mouseTurnMultiplier = 1;
+
+    private float x;
+    private float z;
+
+    void Update()
+    {
+        // x is used for the x axis.  set it to zero so it doesn't automatically rotate
+        x = 0;
+
+        // check to see if the W or S key is being pressed.  
+        z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        // Move the character forwards or backwards
+        transform.Translate(0, 0, z);
+
+        // Check to see if the A or D key are being pressed
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            // Get the A or S key (-1 or 1)
+            x = Input.GetAxis("Horizontal");
+        }
+
+        transform.Translate(x, 0, 0);
+
+        //// Check to see if the right mouse button is pressed
+        //if (Input.GetMouseButton(1))
+        //{
+        //    // Get the difference in horizontal mouse movement
+        //    x = Input.GetAxis("Mouse X") * turnSpeed * mouseTurnMultiplier;
+        //}
+
+        //// rotate the character based on the x value
+        //transform.Rotate(0, x, 0);
+    }
+}
